a00f7643c73501f4263924df7e784b9f5a07b5b0
manda <manda@DESKTOP-EO2AFDN>
2021-11-11 16:26:12 +0300

    Добавьте файлы проекта.

---
+++ Привет мир/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Привет_мир
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
